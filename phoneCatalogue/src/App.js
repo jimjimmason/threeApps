c11f9bf6e7f4e80257399d2d2ae6caf9bbcc3f00
jimjimmason <jmason@eircom.net>
2017-02-18 19:09:28 +0000

    Phone Catalog - Static version completed

--- phoneCatalogue/src/App.js
+++ phoneCatalogue/src/App.js
@@ -1,53 +1,72 @@
 import React from 'react';
+import './App.css'
 
-    var SelectBox = React.createClass({
-      render: function(){
-           return (
-             <div className="col-md-10">
-            <input type="text" placeholder="Search" />
-            Sort by:
-            <select>
-              <option value="name">Alphabetical</option>
-              <option value="age">Newest</option>
-            </select>
-             </div>
-            );
-          }
-       });
+var SelectBox = React.createClass({
+  render: function(){
+    return (
+      <div className="col-md-10">
+      <input type="text" placeholder="Search" />
+      Sort by:
+      <select>
+        <option value="name">Alphabetical</option>
+        <option value="age">Newest</option>
+      </select>
+      </div>
+    );
+  }
+});
+
+// TODO (missing component)
+var PhoneItem = React.createClass({
+  render: function(){
+    //console.log('PhoneItem');
+    //console.log(this.props.phone.name);
+    return(
+      <li className="thumbnail phone-listing">
+        <a href={this.props.phone.id} className="thumb">
+          <img src={this.props.phone.imageUrl} alt={this.props.phone.name} />
+        </a>
+        <a href={this.props.phone.id}>{this.props.phone.name}</a>
+        <p>{this.props.phone.snippet}</p>
+      </li>
+    );
+  }
+});
+
+var FilteredPhoneList = React.createClass({
+  render: function(){
+    console.log('FilteredPhoneList');
+    var displayedPhones = this.props.phones.map(function(phone) {
+      return <PhoneItem key={phone.id} phone={phone} /> ;
+    }) ;
+    return (
+      <div className="col-md-10">
+        <ul className="phones">
+          {displayedPhones}
+        </ul>
+      </div>
+    ) ;
+  }
+});
 
 
-        // TODO (missing component)
+var PhoneCatalogueApp = React.createClass({
+  render: function(){
+    console.log('PhoneCatalogueApp');
+    //console.log(this.props.phones);
+    return (
+      <div className="view-container">
+        <div className="view-frame">
+          <div className="container-fluid">
+            <div className="row">
+              <SelectBox  />
+              <FilteredPhoneList phones={this.props.phones}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
-       var FilteredPhoneList = React.createClass({
-            render: function(){
-                var displayedPhones = this.props.phones.map(function(phone) {
-                  return <PhoneItem key={phone.id} phone={phone} /> ;
-                }) ;
-                return (
-                        <div className="col-md-10">
-                          <ul className="phones">
-                              {displayedPhones}
-                          </ul>
-                        </div>
-                  ) ;
-            }
-        });
-
-    var PhoneCatalogueApp = React.createClass({
-      render: function(){
-          return (
-              <div className="view-container">
-              <div className="view-frame">
-                 <div className="container-fluid">
-                   <div className="row">
-                       <SelectBox  />
-                       {/* TODO */}
-                  </div>
-                  </div>
-                </div>
-              </div>
-          );
-      }
-    });
-
-    export default PhoneCatalogueApp;
+export default PhoneCatalogueApp;
--- phoneCatalogue/src/index.js
+++ phoneCatalogue/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-    import ReactDOM from 'react-dom';
-    import PhoneCatalogueApp from './App';
-    import '../node_modules/bootstrap/dist/css/bootstrap.css';
-    import Phones from  './Data';
+import ReactDOM from 'react-dom';
+import PhoneCatalogueApp from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Phones from  './Data';
 
-    ReactDOM.render(
-      <PhoneCatalogueApp phones={Phones}/>,
-      document.getElementById('root')
-    );
+
+ReactDOM.render(
+  <PhoneCatalogueApp phones={Phones}/>,
+  document.getElementById('root')
+);
