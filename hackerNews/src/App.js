4e5ee1764ea8a4babf75bae346f4cd458527ed7e
jimjimmason <jmason@eircom.net>
2017-02-18 19:32:52 +0000

    HackerNews - Static version - no dataset rendered

--- hackerNews/src/App.js
+++ hackerNews/src/App.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+var Form = React.createClass({
+  render : function() {
+    console.log('Form');
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <form style={{marginTop: '30px'}}>
+        <h3>Add a new post</h3>
+
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Title"></input>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Link"></input>
+        </div>
+        <button type="submit" className="btn btn-primary">Post</button>
+      </form>
+    );
+  }
+});
+
+var NewsItem = React.createClass({
+  render : function() {
+    console.log('NewsItem');
+    var divStyle = {
+      fontSize: '20px',
+      marginLeft: '10px'
+    };
+    var cursor = { cursor: 'pointer' } ;
+    var line ;
+    if (this.props.post.link ) {
+      line = <a href={this.props.post.link} >
+      {            this.props.post.title} </a> ;
+    } else {
+      line = <span>{this.props.post.title} </span> ;
+    }
+    return (
+      <div >
+        <span className="glyphicon glyphicon-thumbs-up"
+        style={cursor} />
+        {this.props.post.upvotes}
+        <span style={divStyle}>{line}
+          <span>
+            <a href={'#/posts/' + this.props.post.id }>Comments</a>
+          </span>
+        </span>
       </div>
     );
   }
-}
+}) ;
 
-export default App;
+// TODO (missing component)
+
+var HackerApp = React.createClass({
+  render: function(){
+    console.log('HackerApp');
+    console.log(this.props.posts);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <div className="page-header">
+              {/* TODO */}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default HackerApp;
